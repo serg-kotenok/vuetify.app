0cfdc91ef349ef2ef142923d56cf1cb6f5048514
Sergey <sergey.kotenok@protobase.ru>
2019-06-02 11:42:10 +0300

    everything prepared

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
@@ -12,7 +12,7 @@ Vue.use(Vuetify)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  //  router,
   components: { App },
   template: '<App/>'
 })
