5b4ef4ea8978213146d47cc40d481e309e58e560
Sergey Kotenok <serg.kotenok@gmail.com>
2019-06-22 09:28:59 +0300

    Login Form Done

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,9 @@
 <template>
   <v-app dark>
-    <!--
-    <v-navigation-drawer app temporary hideOverlay></v-navigation-drawer>
-    <v-toolbar app>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
-      <v-toolbar-title>Health Diary</v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-toolbar>
--->
     <navbar></navbar>
-<!--
     <v-content>
-      <v-container fluid>
-        <v-flex xs1>
-          <router-view></router-view>
-        </v-flex>
-      </v-container>
+      <router-view></router-view>
     </v-content>
--->
     <v-footer app></v-footer>
   </v-app>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Register from '@/components/user/Register'
+import Login from '@/components/user/Login'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
-    { path: '/', component: Home }
+    { path: '', name: 'home', component: Home },
+    { path: '/login', name: 'login', component: Login },
+    { path: '/register', name: 'register', component: Register }
   ]
 })
